2b2883183c6c79e92960ff386fca1b4af67608c1
Choco0914 <heojeongho1992@gmail.com>
2018-08-25 16:06:52 +0900

    view를 추가했다

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+"""
+사용자가 웹사이트의 페이지를 요청할 때, 예로 /"polls/34/"를 요청했다 하면,
+Django는 mysite.urls 파이썬 모듈을 불러오게 된다 ROOT_URLCONF 설정에 의해 해당
+모듀을 바라보도록 지정되어 있기 때문이다 mysite.urls에서 urlpatterns라는 변수를
+찾고, 순서대로 패턴을 따라간다 'polls/'를 찾은 후엔, 일치하는 텍스트("polls/")를
+버리고, 남은 텍스트인 "34/"를 'polls.urls'URLconf로 전달하여 남은처리를 진행한다
+거기에 '<int:question_id>/'와 일치하여, 결과적으로 detail()view함수를 호출한다
+-다음은 views.py detail 함수부분의 주석을 참고-
+"""
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,36 @@
 from django.http import HttpResponse
-
+# HttpREsponse는 사용자가 원하는 페이지의 요청을 반환해주는 역할을해준다
+from django.template import loader
+"""
+Django는 project의 TEMPLATES설정에 의해 template을 어떻게 불러오고 렌더링할지
+알고있다
+"""
+from .models import Question
+# 데이터베이스의 레코드를 읽기위해 models의 Question클래스를 임포트했다
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# 뷰에 request가 오게되면 view에서 HttpResponse를 반환하게된다
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    """
+    polls/index.html template 을 불러온 후, context를 전달한다 context는
+    template 에서 쓰이는 변수명과, Python 의 객체를 연결하는 사전형 값이다
+    """
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+"""
+detail(request=<HttpREquest object>, question_id=34)
+question_id=34 부분은 <int:question_id>에서 왔다 괄호를 사용하여 URL의 일부를
+"캡쳐"하고, 해당 내용을 keyward 인수로서 view함수로 전달한다 문자열의:question_id>
+부분은 일치되는 패턴을 구별하기 위해 정의한 이름이며, <int:부분은 어느 패턴이
+해당 URL 경로에 일치되어야 하는지를 결정하는 컨버터 이다
+"""
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
